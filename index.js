57da08690f0a5cc092a392bc540f6e4023567f7c
aditya <vatadepalli@hotmail.com>
2021-06-23 15:49:12 +0530

    add filter based on vaccine type

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const calcTime = (offset) => {
 
 const handler = async (event) => {
 	const _date = calcTime("+5.5");
+	const CHAT_ID = "";
+	const BOT_API_KEY = "";
 
 	const _res = await new Promise(async (resolve, reject) => {
 		axios
@@ -39,14 +41,15 @@ const handler = async (event) => {
 
 				out = out
 					.filter((el) => el.min_age_limit === 18)
-					.filter((el) => el.available_capacity_dose1 > 0);
+					.filter((el) => el.available_capacity_dose1 > 0)
+					.filter((el) => el.vaccine === "COVAXIN");
 
 				if (new Date().getMinutes() % 10 === 0) {
 					await new Promise((res, rej) => {
 						// Interval Pings
 						axios
 							.get(
-								`https://api.telegram.org/bot<BOT_API_KEY>/sendMessage?chat_id=<CHAT_ID>&text=Test Ping`
+								`https://api.telegram.org/bot${BOT_API_KEY}/sendMessage?chat_id=${CHAT_ID}&text=Interval Ping`
 							)
 							.then(({ data }) => {
 								console.log(data);
@@ -63,7 +66,7 @@ const handler = async (event) => {
 						// Positive Responses
 						axios
 							.get(
-								`https://api.telegram.org/bot<BOT_API_KEY>/sendMessage?chat_id=<CHAT_ID>&text=${out.length} slots available in Nasik for dose 1. Checked on ${_date}. Showing for ${dates}`
+								`https://api.telegram.org/bot${BOT_API_KEY}/sendMessage?chat_id=${CHAT_ID}&text=${out.length} slot(s) available in Nasik for dose 1 of COVAXIN. Checked on ${_date}. Showing for ${dates}`
 							)
 							.then(({ data }) => {
 								console.log(data);
@@ -90,3 +93,5 @@ const handler = async (event) => {
 };
 
 exports.handler = handler;
+
+// handler();
